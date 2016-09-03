01cbf2d5b94506222401eac9c19e78ac74d0a81c
nicholashaynes1 <nicholashaynes1@gmail.com>
2016-09-03 17:43:59 -0600

    Added a retrun to main screen button(untested)

--- app/src/main/java/nickhaynes/demo21days/controller/ReadingScreenActivity.java
+++ app/src/main/java/nickhaynes/demo21days/controller/ReadingScreenActivity.java
@@ -1,19 +1,43 @@
 package nickhaynes.demo21days.controller;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import nickhaynes.demo21days.R;
 
 /**
  * The new activity that uses a screen to show the stories.
  */
-public class ReadingScreenActivity extends Activity {
+public class ReadingScreenActivity extends Activity
+{
+    //button decleration.
+    private Button backToMainMenuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reading_screen);
+
+        //button instantiation
+        backToMainMenuButton = (Button) findViewById(R.id.backToMainMenuButton);
+
+        setupListener();
+    }
+    private void setupListener()
+    {
+        backToMainMenuButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View clickView)
+            {
+                Intent returnIntent = new Intent();
+                setResult(RESULT_OK, returnIntent);
+                finish();
+            }
+        });
+
     }
 }
